4e823acf0e7d04bc8ada0980d45cc1b4e499a0c4
trybybass <abcdfriend007@gmail.com>
2024-08-01 12:06:35 +0200

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import sys
-if len(sys.argv) == 2:
-    if sys.maxsize > 2*32:
-        file = sys.argv[1]
-        import Bypass
-        Bypass.Bypass(file)
-    else:
-        print("YOUR SYSTEM IS 32bit")
-        sys.exit()
